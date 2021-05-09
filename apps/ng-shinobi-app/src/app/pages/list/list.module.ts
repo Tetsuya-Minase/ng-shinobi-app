e11f98cf4a6ea89d23a3e347ac1d8056cac074ac
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-05-09 13:28:16 +0900

    feat: list page template

---
+++ apps/ng-shinobi-app/src/app/pages/list/list.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { ListComponent } from './list.component';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [ListComponent],
+  providers: []
+})
+export class ListModule {
+}
+
