fc4af7139da817cbcdb40bda494b87ec6080134f
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-05-09 13:33:22 +0900

    refactor: component name🔧

---
+++ apps/ng-shinobi-app/src/app/pages/character-list/character-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ng-shinobi-app-character-list-page',
+  templateUrl: 'character-list.component.html'
+})
+
+export class CharacterListComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
---
+++ apps/ng-shinobi-app/src/app/pages/character-list/character-list.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { CharacterListComponent } from './character-list.component';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [CharacterListComponent],
+  providers: []
+})
+export class CharacterListModule {
+}
+
