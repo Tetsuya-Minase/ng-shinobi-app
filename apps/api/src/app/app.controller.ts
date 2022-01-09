f88d047e5d3d39e4f479240c11b62ac72fec6cc8
Tetsuya-Minase <tetsuya.minasen@gmail.com>
2022-01-09 19:14:36 +0900

    feat: add header component

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,15 +1,45 @@
 import { Controller, Get } from '@nestjs/common';
 
-import { Message } from '@ng-shinobi-app/api-interfaces';
+import {CharacterListResponse, Message} from '@ng-shinobi-app/api-interfaces';
 
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/v1')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('hello')
   getData(): Message {
     return this.appService.getData();
+  }
+  @Get('character')
+  public getCharacter(): CharacterListResponse {
+    return {
+      start: 1,
+      total: 10,
+      result: [
+        {
+          userId: 'hoge',
+          playerName: 'hoge',
+          name: 'hoge',
+          kana: 'hoge',
+          regulation: 'hoge',
+          type: 'hoge',
+          ryuha: 'hoge',
+          kairyuha: 'hoge',
+          ryugi: 'hoge',
+          enemy: 'hoge',
+          rank: 'hoge',
+          achievement: 'hoge',
+          age: 'hoge',
+          gender: 'hoge',
+          face: 'hoge',
+          belief: 'hoge',
+          hyorogan: 100,
+          jintugan: 10,
+          tonkohu: 100,
+        }
+      ]
+    }
   }
 }
